18a1d025abd810d222d8b8f9627b47ddcd840d4d
KoPhaiVu <hoangle293vu@gmail.com>
2024-03-27 22:54:11 +0700

    first commit

---
+++ Lab2_22521691/Lab2_22521691/Task1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab2_22521691
+{
+    public partial class Task1 : Form
+    {
+        public Task1()
+        {
+            InitializeComponent();
+        }
+
+        private void Task1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void readFileBtn_Click(object sender, EventArgs e)
+        {
+            StreamWriter write = new StreamWriter(fileName.Text, true);
+            write.WriteLine(fileData.Text);
+            write.Close();
+        }
+
+        private void writeFileBtn_Click(object sender, EventArgs e)
+        {
+            StreamReader read = new StreamReader(fileName.Text);
+            fileData.Text = read.ReadToEnd();
+            read.Close();
+        }
+
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            if (MessageBox("Bạn có muốn thoát?", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == )
+            {
+                this.Hide();
+                MainForm mf = new MainForm();
+                mf.ShowDialog();
+                this.Close();
+            }
+        }
+
+        private void deleteBtn_Click(object sender, EventArgs e)
+        {
+            fileName.Clear();
+        }
+    }
+}
---
+++ Lab2_22521691/Lab2_22521691/Task5.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab2_22521691
+{
+    public partial class Task5 : Form
+    {
+        public Task5()
+        {
+            InitializeComponent();
+        }
+    }
+}
