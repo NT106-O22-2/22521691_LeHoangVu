5beb51d1d08f7adba11d76d3d356fff357e2fd98
Pttvinh253 <[200~22521680@gm.uit.edu.vn>
2024-05-03 23:14:25 +0700

    Bai3

--- Lab4_22521691_22521387_22521680/Task3/Task2.Designer.cs
+++ Lab4_22521691_22521387_22521680/Task3/Task2.Designer.cs
@@ -28,94 +28,154 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.loadBtn = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.downFileBtn = new System.Windows.Forms.Button();
-            this.downResourcesBtn = new System.Windows.Forms.Button();
-            this.webView = new Microsoft.Web.WebView2.WinForms.WebView2();
-            ((System.ComponentModel.ISupportInitialize)(this.webView)).BeginInit();
+            this.webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.btnViewSource = new System.Windows.Forms.Button();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.btnDownFiles = new System.Windows.Forms.Button();
+            this.btnDownResources = new System.Windows.Forms.Button();
+            this.btnReload = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).BeginInit();
             this.SuspendLayout();
             // 
-            // loadBtn
+            // webView21
             // 
-            this.loadBtn.Font = new System.Drawing.Font("UTM Avo", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.loadBtn.Location = new System.Drawing.Point(577, 10);
-            this.loadBtn.Name = "loadBtn";
-            this.loadBtn.Size = new System.Drawing.Size(128, 35);
-            this.loadBtn.TabIndex = 9;
-            this.loadBtn.Text = "Load";
-            this.loadBtn.UseVisualStyleBackColor = true;
-            this.loadBtn.Click += new System.EventHandler(this.loadBtn_Click);
+            this.webView21.AllowExternalDrop = true;
+            this.webView21.CreationProperties = null;
+            this.webView21.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView21.Location = new System.Drawing.Point(22, 131);
+            this.webView21.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.webView21.Name = "webView21";
+            this.webView21.Size = new System.Drawing.Size(788, 513);
+            this.webView21.TabIndex = 13;
+            this.webView21.ZoomFactor = 1D;
             // 
-            // textBox1
+            // btnViewSource
             // 
-            this.textBox1.Font = new System.Drawing.Font("UTM Avo", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(12, 10);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(559, 35);
-            this.textBox1.TabIndex = 8;
+            this.btnViewSource.BackColor = System.Drawing.Color.LightCyan;
+            this.btnViewSource.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.btnViewSource.FlatAppearance.CheckedBackColor = System.Drawing.Color.LightSkyBlue;
+            this.btnViewSource.FlatAppearance.MouseDownBackColor = System.Drawing.Color.LightSkyBlue;
+            this.btnViewSource.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Azure;
+            this.btnViewSource.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnViewSource.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnViewSource.Location = new System.Drawing.Point(306, 65);
+            this.btnViewSource.Name = "btnViewSource";
+            this.btnViewSource.Size = new System.Drawing.Size(135, 45);
+            this.btnViewSource.TabIndex = 19;
+            this.btnViewSource.Text = "View Source ";
+            this.btnViewSource.UseVisualStyleBackColor = false;
+            this.btnViewSource.Click += new System.EventHandler(this.btnViewSource_Click);
             // 
-            // downFileBtn
+            // txtUrl
             // 
-            this.downFileBtn.Font = new System.Drawing.Font("UTM Avo", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.downFileBtn.Location = new System.Drawing.Point(377, 51);
-            this.downFileBtn.Name = "downFileBtn";
-            this.downFileBtn.Size = new System.Drawing.Size(140, 35);
-            this.downFileBtn.TabIndex = 11;
-            this.downFileBtn.Text = "Down Files";
-            this.downFileBtn.UseVisualStyleBackColor = true;
-            this.downFileBtn.Click += new System.EventHandler(this.downFileBtn_Click);
+            this.txtUrl.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUrl.Location = new System.Drawing.Point(114, 17);
+            this.txtUrl.Multiline = true;
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(548, 35);
+            this.txtUrl.TabIndex = 18;
+            this.txtUrl.Text = "https://uit.edu.vn";
             // 
-            // downResourcesBtn
+            // btnDownFiles
             // 
-            this.downResourcesBtn.Font = new System.Drawing.Font("UTM Avo", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.downResourcesBtn.Location = new System.Drawing.Point(523, 51);
-            this.downResourcesBtn.Name = "downResourcesBtn";
-            this.downResourcesBtn.Size = new System.Drawing.Size(182, 35);
-            this.downResourcesBtn.TabIndex = 12;
-            this.downResourcesBtn.Text = "Down Resources";
-            this.downResourcesBtn.UseVisualStyleBackColor = true;
-            this.downResourcesBtn.Click += new System.EventHandler(this.downResourcesBtn_Click);
+            this.btnDownFiles.BackColor = System.Drawing.Color.LightCyan;
+            this.btnDownFiles.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.btnDownFiles.FlatAppearance.CheckedBackColor = System.Drawing.Color.LightSkyBlue;
+            this.btnDownFiles.FlatAppearance.MouseDownBackColor = System.Drawing.Color.LightSkyBlue;
+            this.btnDownFiles.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Azure;
+            this.btnDownFiles.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDownFiles.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDownFiles.Location = new System.Drawing.Point(454, 65);
+            this.btnDownFiles.Name = "btnDownFiles";
+            this.btnDownFiles.Size = new System.Drawing.Size(125, 45);
+            this.btnDownFiles.TabIndex = 17;
+            this.btnDownFiles.Text = "Down Files";
+            this.btnDownFiles.UseVisualStyleBackColor = false;
+            this.btnDownFiles.Click += new System.EventHandler(this.btnDownFiles_Click);
             // 
-            // webView
+            // btnDownResources
             // 
-            this.webView.AllowExternalDrop = true;
-            this.webView.CreationProperties = null;
-            this.webView.DefaultBackgroundColor = System.Drawing.Color.White;
-            this.webView.Location = new System.Drawing.Point(12, 91);
-            this.webView.Name = "webView";
-            this.webView.Size = new System.Drawing.Size(693, 570);
-            this.webView.TabIndex = 13;
-            this.webView.ZoomFactor = 1D;
+            this.btnDownResources.BackColor = System.Drawing.Color.LightCyan;
+            this.btnDownResources.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.btnDownResources.FlatAppearance.CheckedBackColor = System.Drawing.Color.LightSkyBlue;
+            this.btnDownResources.FlatAppearance.MouseDownBackColor = System.Drawing.Color.LightSkyBlue;
+            this.btnDownResources.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Azure;
+            this.btnDownResources.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDownResources.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDownResources.Location = new System.Drawing.Point(595, 65);
+            this.btnDownResources.Name = "btnDownResources";
+            this.btnDownResources.Size = new System.Drawing.Size(215, 45);
+            this.btnDownResources.TabIndex = 16;
+            this.btnDownResources.Text = "Down Resources";
+            this.btnDownResources.UseVisualStyleBackColor = false;
+            this.btnDownResources.Click += new System.EventHandler(this.btnDownResources_Click);
+            // 
+            // btnReload
+            // 
+            this.btnReload.BackColor = System.Drawing.Color.LightCyan;
+            this.btnReload.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.btnReload.FlatAppearance.MouseDownBackColor = System.Drawing.Color.LightSkyBlue;
+            this.btnReload.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Azure;
+            this.btnReload.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReload.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReload.Location = new System.Drawing.Point(684, 11);
+            this.btnReload.Name = "btnReload";
+            this.btnReload.Size = new System.Drawing.Size(127, 45);
+            this.btnReload.TabIndex = 15;
+            this.btnReload.Text = "Reload";
+            this.btnReload.UseVisualStyleBackColor = false;
+            this.btnReload.Click += new System.EventHandler(this.btnReload_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.BackColor = System.Drawing.Color.LightCyan;
+            this.btnLoad.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.btnLoad.FlatAppearance.MouseDownBackColor = System.Drawing.Color.LightSkyBlue;
+            this.btnLoad.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Azure;
+            this.btnLoad.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLoad.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLoad.Location = new System.Drawing.Point(22, 14);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 45);
+            this.btnLoad.TabIndex = 14;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = false;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
             // 
             // Task2
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = global::Task3.Properties.Resources.Task2;
+            this.BackColor = System.Drawing.Color.AliceBlue;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(719, 666);
-            this.Controls.Add(this.webView);
-            this.Controls.Add(this.downResourcesBtn);
-            this.Controls.Add(this.downFileBtn);
-            this.Controls.Add(this.loadBtn);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(822, 657);
+            this.Controls.Add(this.btnViewSource);
+            this.Controls.Add(this.txtUrl);
+            this.Controls.Add(this.btnDownFiles);
+            this.Controls.Add(this.btnDownResources);
+            this.Controls.Add(this.btnReload);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.webView21);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Task2";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Task3";
-            ((System.ComponentModel.ISupportInitialize)(this.webView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.Button loadBtn;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button downFileBtn;
-        private System.Windows.Forms.Button downResourcesBtn;
-        private Microsoft.Web.WebView2.WinForms.WebView2 webView;
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
+        private System.Windows.Forms.Button btnViewSource;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Button btnDownFiles;
+        private System.Windows.Forms.Button btnDownResources;
+        private System.Windows.Forms.Button btnReload;
+        private System.Windows.Forms.Button btnLoad;
     }
 }
 
--- Lab4_22521691_22521387_22521680/Task3/Task3.cs
+++ Lab4_22521691_22521387_22521680/Task3/Task3.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using HtmlAgilityPack;
 
 namespace Task3
 {
@@ -16,30 +20,90 @@ namespace Task3
         {
             InitializeComponent();
         }
-
-        private void loadBtn_Click(object sender, EventArgs e)
+        private string getHTML(string szURL)
         {
-            if (!string.IsNullOrEmpty(textBox1.Text))
+            //Creata a request for the URL.
+            WebRequest requestt = WebRequest.Create(szURL);
+            //Get the response.
+            WebResponse response = requestt.GetResponse();
+            //Get the stream containing content returned by the server.
+            Stream dataStream = response.GetResponseStream();
+            //Open the stream using a StreamReader for easy access.
+            StreamReader reader = new StreamReader(dataStream);
+            //Read the content.
+            string responseFromServer = reader.ReadToEnd();
+            //Close the response.
+            reader.Close();
+            return responseFromServer;
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            string url = txtUrl.Text;
+            webView21.Source = new Uri(url);
+        }
+
+        private void btnReload_Click(object sender, EventArgs e)
+        {
+            webView21.Reload();
+        }
+
+        private void btnViewSource_Click(object sender, EventArgs e)
+        {
+            var html = getHTML(this.txtUrl.Text);
+            var form = new Form();
+            form.Text = "HTML Source";
+            form.Width = 800;
+            form.Height = 600;
+
+            var txtBox = new RichTextBox();
+            txtBox.Dock = DockStyle.Fill;
+            txtBox.Multiline = true;
+            txtBox.ScrollBars = RichTextBoxScrollBars.Vertical;
+            txtBox.Text = html;
+
+            form.Controls.Add(txtBox);
+            form.ShowDialog();
+        }
+
+        private void btnDownFiles_Click(object sender, EventArgs e)
+        {
+
+            HtmlWeb web = new HtmlWeb();
+            var htmlDoc = web.Load(txtUrl.Text);
+
+            var html = htmlDoc.DocumentNode.OuterHtml;
+            var htmlFileName = "website.html";
+            var htmlFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, htmlFileName);
+
+            File.WriteAllText(htmlFilePath, html);
+
+            MessageBox.Show($"Tải HTML của trang web thành công và lưu vào {htmlFileName}!");
+        }
+
+        private void btnDownResources_Click(object sender, EventArgs e)
+        {
+            HtmlWeb web = new HtmlWeb();
+            var htmlDoc = web.Load(txtUrl.Text);
+
+            var imgs = htmlDoc.DocumentNode.SelectNodes("//img");
+            if (imgs != null)
             {
-                try
+                using (var client = new HttpClient())
                 {
-                    webView.Source = new Uri(textBox1.Text);
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show("Error loading URL: " + ex.Message);
+                    foreach (var img in imgs)
+                    {
+                        var imgURL = img.GetAttributeValue("src", "");
+
+                        MessageBox.Show(imgURL);
+                    }
+                    MessageBox.Show("Tải tất cả hình ảnh thành công!");
                 }
             }
-        }
-
-        private void downFileBtn_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void downResourcesBtn_Click(object sender, EventArgs e)
-        {
-
+            else
+            {
+                MessageBox.Show("Không có hình ảnh nào để tải!");
+            }
         }
     }
 }
